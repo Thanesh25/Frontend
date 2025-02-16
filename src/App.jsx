c1c464b43962ff09daa95db68cc8d57a7a9a0eec
Thaneshwar <Thaneshwar.js@gmail.com>
2025-02-16 23:35:31 +0530

    changed the value

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import AllRouters from './routers/AllRouters'
 import { Header, Footer } from "./Components"
-import axios from "axios";
+import axios from "axios"
+
 
 axios.defaults.baseURL = "https://backend-0er4.onrender.com/";
-axios.defaults.withCredentials = true;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.withCredentials="true"
 
 function App() 
 {
+   console.log(axios.defaults.baseURL);
+   console.log(axios.defaults.withCredentials);
  
 
   return (
